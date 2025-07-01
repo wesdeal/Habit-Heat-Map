5747409188008fa3a3eaa3454bdb560d92ad5891
Wesley Deal <wesdeall@gmail.com>
2025-07-01 00:42:20 -0400

    connected supabase to sign up form; email passes through

--- src/frontend/SignUp.jsx
+++ src/frontend/SignUp.jsx
@@ -1,7 +1,14 @@
 import Navbar from "./Navbar";
+import UseSignUp from "./useSignUp";
 
-export default function SignUp() {
+export default function signUp() {
+    
 
+    const {
+        email,
+        setEmail,
+        handleRegister
+    } = UseSignUp()
 
     return(
         <div>
@@ -9,13 +16,13 @@ export default function SignUp() {
             <form action="">
                 <div className="textInput">
                     <input type="text" id="username" placeholder="Create a username..." />
-                    <input type="text" id="email" placeholder="Email..." />
+                    <input type="text" id="email" value={email} onChange={(e) => setEmail(e.target.value)} placeholder="Email..." />
                     <input type="password" id="password" placeholder="Password..." />
                     <input type="password" id="cPassword" placeholder="Confirm Password..." />
                 </div>
 
                 <div className="registerBtn">
-                    <button>Register</button>
+                    <button className="register-btn" onClick={handleRegister} >Register</button>
                 </div>
                 
             </form>
--- src/frontend/UseSignUp.js
+++ src/frontend/UseSignUp.js
@@ -1,8 +1,33 @@
 import supabase from '../supabaseClient'
+import { useState } from 'react'
 
 
-function handleRegister() {
-    /* logic for when a user hits submit on sign up form */
-
+export default function UseSignUp() {
     
+    const [email, setEmail] = useState("")
+
+    async function handleRegister(e) {
+        /* logic for when a user hits submit on sign up form */
+
+        e.preventDefault();
+
+        const { data, error } = await supabase
+            .from('Users')
+            .insert([{email: email, username: username}])
+        
+        /* so far, this does successfully add email to database and give user a UUID */
+        /* 
+            to add:
+            once user hits register button, they shall be redirected back to homepage, logged in
+
+            also check for if an email or username alrady exists
+        */
+    }
+    
+
+    return {
+        email,
+        setEmail,
+        handleRegister
+    };
 }
